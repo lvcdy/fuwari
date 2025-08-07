56c5e1418155af659cfb4259f87c9f44a76ca4d2
lvcdy <243457263@qq.com>
2025-08-07 18:26:50 +0800

    change

--- src/config.ts
+++ src/config.ts
@@ -50,6 +50,11 @@ export const navBarConfig: NavBarConfig = {
 			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
+		{
+			name: "统计",
+			url: "https://umami.cunique.top/share/ZOxKDi7465e8d3XO/www.cunique.top",
+			external: true,
+		},
 	],
 };
 
